56f6f0392613e13d979d4a8a9601755b2554786b
twenty-one-savage <alohafrombali@gmail.com>
2020-10-03 20:57:08 +0300

    2.3 Рефакторинг JSX и добавил динамический роутинг

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import PropTypes from "prop-types";
-import Main from "../main/main";
+import MainScreen from "../main-screen/main-screen";
+import AddReviewScreen from "../add-review-screen/add-review-screen";
+import FilmScreen from "../film-screen/film-screen";
+import MyListScreen from "../my-list-screen/my-list-screen";
+import PlayerScreen from "../player-screen/player-screen";
+import SignInScreen from "../sign-in-screen/sign-in-screen";
 
 const App = (props) => {
   const {name, genre, year} = props;
@@ -9,27 +14,21 @@ const App = (props) => {
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
-          <Main
+          <MainScreen
             name={name}
             genre={genre}
             year={year}
           />
         </Route>
         <Route exact path="/login">
-
+          <SignInScreen />
         </Route>
         <Route exact path="/mylist">
-
+          <MyListScreen />
         </Route>
-        <Route exact path="/films/:id">
-
-        </Route>
-        <Route exact path="/films/:id/review">
-
-        </Route>
-        <Route exact path="/player/:id">
-
-        </Route>
+        <Route exact path="/films/:id/review" component={AddReviewScreen} />
+        <Route path="/films/:id?" exact component={FilmScreen} />
+        <Route exact path="/player/:id" component={PlayerScreen} />
       </Switch>
     </BrowserRouter>
   );
--- src/components/main-screen/main-screen.jsx
+++ src/components/main-screen/main-screen.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Main = (props) => {
+const MainScreen = (props) => {
   const {name, genre, year} = props;
   return (
     <>
@@ -76,13 +76,13 @@ const Main = (props) => {
               <div className="movie-card__buttons">
                 <button className="btn btn--play movie-card__button" type="button">
                   <svg viewBox="0 0 19 19" width="19" height="19">
-                    <use xlinkHref="#play-s"></use>
+                    <use xlinkHref="#play-s" />
                   </svg>
                   <span>Play</span>
                 </button>
                 <button className="btn btn--list movie-card__button" type="button">
                   <svg viewBox="0 0 19 20" width="19" height="20">
-                    <use xlinkHref="#add"></use>
+                    <use xlinkHref="#add" />
                   </svg>
                   <span>My list</span>
                 </button>
@@ -340,10 +340,10 @@ const Main = (props) => {
   );
 };
 
-Main.propTypes = {
+MainScreen.propTypes = {
   name: PropTypes.string.isRequired,
   genre: PropTypes.string.isRequired,
   year: PropTypes.number.isRequired
 };
 
-export default Main;
+export default MainScreen;
