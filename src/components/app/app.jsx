24dd4c5bcbd8837607433a36041b4e8428774b03
twenty-one-savage <alohafrombali@gmail.com>
2020-10-03 13:04:23 +0300

    2.2 Подключил React-роутер

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,37 @@
 import React from "react";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import PropTypes from "prop-types";
 import Main from "../main/main";
 
 const App = (props) => {
   const {name, genre, year} = props;
   return (
-    <Main
-      name={name}
-      genre={genre}
-      year={year}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main
+            name={name}
+            genre={genre}
+            year={year}
+          />
+        </Route>
+        <Route exact path="/login">
+
+        </Route>
+        <Route exact path="/mylist">
+
+        </Route>
+        <Route exact path="/films/:id">
+
+        </Route>
+        <Route exact path="/films/:id/review">
+
+        </Route>
+        <Route exact path="/player/:id">
+
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
