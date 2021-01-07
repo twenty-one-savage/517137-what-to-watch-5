e294dc85a849aff748a1bd247c056eca068a53da
twenty-one-savage <alohafrombali@gmail.com>
2021-01-07 15:18:02 +0300

    переделал hoc из классового в функциональный

--- src/hocs/with-hover/with-hover.jsx
+++ src/hocs/with-hover/with-hover.jsx
@@ -1,46 +1,75 @@
-const withHover = (Component) => {
-  class WithHover extends React.PureComponent {
-    constructor(props) {
-      super(props);
-      this.state = {
-        isPlaying: false
-      };
-      this._timeoutId = null;
-      this._handleVideoMouseOver = this._handleVideoMouseOver.bind(this);
-      this._handleVideoMouseOut = this._handleVideoMouseOut.bind(this);
-    }
+// const withHover = (Component) => {
+//   class WithHover extends React.PureComponent {
+//     constructor(props) {
+//       super(props);
+//       this.state = {
+//         isPlaying: false
+//       };
+//       this._timeoutId = null;
+//       this._handleVideoMouseOver = this._handleVideoMouseOver.bind(this);
+//       this._handleVideoMouseOut = this._handleVideoMouseOut.bind(this);
+//     }
+//
+//     componentWillUnmount() {
+//       clearTimeout(this._timeoutId);
+//     }
+//
+//     _handleVideoMouseOver() {
+//       this._timeoutId = setTimeout(() => {
+//         this.setState({
+//           isPlaying: true
+//         });
+//       }, 1000);
+//     }
+//
+//     _handleVideoMouseOut() {
+//       clearTimeout(this._timeoutId);
+//       this.setState({
+//         isPlaying: false
+//       });
+//     }
+//
+//     render() {
+//       return (
+//         <Component
+//           {...this.props}
+//           isPlaying={this.state.isPlaying}
+//           onMouseOver={this._handleVideoMouseOver}
+//           onMouseOut={this._handleVideoMouseOut}/>
+//       );
+//     }
+//   }
+//
+//   return WithHover;
+// };
+//
 
-    componentWillUnmount() {
-      clearTimeout(this._timeoutId);
-    }
 
-    _handleVideoMouseOver() {
-      this._timeoutId = setTimeout(() => {
-        this.setState({
-          isPlaying: true
-        });
-      }, 1000);
-    }
+const withHover = (Component) => (props) => {
+  const [isPlaying, setPlaying] = React.useState(false);
+  let timeoutId = null;
 
-    _handleVideoMouseOut() {
-      clearTimeout(this._timeoutId);
-      this.setState({
-        isPlaying: false
-      });
-    }
+  React.useEffect(() => clearTimeout(timeoutId));
 
-    render() {
-      return (
-        <Component
-          {...this.props}
-          isPlaying={this.state.isPlaying}
-          onMouseOver={this._handleVideoMouseOver}
-          onMouseOut={this._handleVideoMouseOut}/>
-      );
-    }
-  }
+  const handleVideoMouseOver = () => {
+    timeoutId = setTimeout(() => {
+      setPlaying(true);
+    }, 1000);
+  };
 
-  return WithHover;
+  const handleVideoMouseOut = () => {
+    setPlaying(false);
+  };
+
+
+  return (
+    <Component
+      {...props}
+      isPlaying={isPlaying}
+      onMouseOver={handleVideoMouseOver}
+      onMouseOut={handleVideoMouseOut}
+    />
+  );
 };
 
 export default withHover;
