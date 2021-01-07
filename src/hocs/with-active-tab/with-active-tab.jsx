05808ac6c8c2c48ae7a335a9ba00c02b65723f84
twenty-one-savage <alohafrombali@gmail.com>
2021-01-07 14:53:19 +0300

    переделал hoc из классового в функциональный

--- src/hocs/with-active-tab/with-active-tab.jsx
+++ src/hocs/with-active-tab/with-active-tab.jsx
@@ -1,31 +1,51 @@
 import {TabsType} from "../../consts";
+//
+// const withActiveTab = (Component) => {
+//   return class WithActiveTabs extends React.PureComponent {
+//     constructor(props) {
+//       super(props);
+//       this.state = {
+//         activeTab: TabsType.OVERVIEW
+//       };
+//
+//       this._handleTabClick = this._handleTabClick.bind(this);
+//     }
+//
+//     _handleTabClick(evt) {
+//       evt.preventDefault();
+//       const newActiveTab = evt.target.dataset.tab;
+//       this.setState({
+//         activeTab: newActiveTab
+//       });
+//     }
+//     render() {
+//       return <Component
+//         {...this.props}
+//         activeTab={this.state.activeTab}
+//         tabClickHandler={this._handleTabClick}
+//       />;
+//     }
+//   };
+// };
+//
+// export default withActiveTab;
+//
 
-const withActiveTab = (Component) => {
-  return class WithActiveTabs extends React.PureComponent {
-    constructor(props) {
-      super(props);
-      this.state = {
-        activeTab: TabsType.OVERVIEW
-      };
+const withActiveTab = (Component) => (props) => {
+  const [activeTab, setActiveTab] = React.useState(TabsType.OVERVIEW);
 
-      this._handleTabClick = this._handleTabClick.bind(this);
-    }
-
-    _handleTabClick(evt) {
-      evt.preventDefault();
-      const newActiveTab = evt.target.dataset.tab;
-      this.setState({
-        activeTab: newActiveTab
-      });
-    }
-    render() {
-      return <Component
-        {...this.props}
-        activeTab={this.state.activeTab}
-        tabClickHandler={this._handleTabClick}
-      />;
-    }
+  const handleTabClick = (evt) => {
+    evt.preventDefault();
+    setActiveTab(evt.target.dataset.tab);
   };
+
+  return (
+    <Component
+      {...props}
+      activeTab={activeTab}
+      tabClickHandler={handleTabClick}
+    />
+  );
 };
 
 export default withActiveTab;
